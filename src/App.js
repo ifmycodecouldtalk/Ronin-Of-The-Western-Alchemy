ea476c6a9efc9f24b286dad01fde955ae1133e5e
Aiden Miller <bonsoirelliot@gmail.com>
2022-08-11 13:53:11 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Link
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/js/dist/dropdown';
 import Home from './pages/Home';
 
 export class App extends React.Component {
@@ -13,7 +14,16 @@ export class App extends React.Component {
     return (
       <Router>
         {/* navigation bar goes here */}
-
+        <nav className='navbar navbar-expand bg-dark'>
+          <div className='container'>
+            <a className='navbar-brand text-bg-dark' href='#'>Ronans of the Western Alchemy</a>
+            <ul className='navbar-nav me-auto'>
+              <li className='nav-item'>
+                <a className='nav-link active text-bg-dark' href='#'>Docs</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
         {/* pages rendered under the navbar */}
         <Routes>
