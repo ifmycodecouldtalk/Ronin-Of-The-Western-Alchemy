f66f645fd7a684ed791df2d4f982c9376be3cdea
ifmycodecouldtalk <bonsoirelliot0101@gmail.com>
2022-08-17 19:59:10 -0400

    reformatting of content

--- src/pages/Oscp-helpers.js
+++ src/pages/Oscp-helpers.js
@@ -1,129 +1,126 @@
 const helpers ={
     renderPage(page) {
         switch(page){
-            case '1.1':
+            case 'introductions':
                 return (
-                    <div className="chapter-01-content helvetica-text">
-                        <h2>1.1: About the PWK Course</h2>
-                        <br />
-                        <h3 className="subheading"><i>PWK Course Materials</i></h3>
-                            <p>The course comes with a lab guide and accompanying course videos. As this
-                                is proprietary property of Offensive Security, I can only share my notes 
-                                from the lab PDF. I will also try and include course exercises at the end of 
-                                every chapter.
-                            </p>
-                        <br />   
-                        <h3 className="subheading"><i>Access to the Internal VPN Lab Network</i></h3>
-                            <p>When you buy the course, you will receive an email welcome with your VPN credentials
-                                and the corresponding VPN connectivity pack. 
-                                <br />
-                                Lab time starts when your course begins and is metered as continuous access. Lab time 
-                                can only be paused by case of emergency.
-                                <br />
-                                If your lab time is expired or about to expire, you can purchase a lab extension
-                                at any time. Use the personalized purchase link in the email. If you purchase a 
-                                lab extension <i>before expiration</i>, you can still use the same VPN connectivity pack;
-                                otherwise, you will get a new VPN connectivity pack.
+                    <div className="introductions-page helvetica-text page">
+                        <h1>Introductions</h1><br />
+                            <p>Hello! My name is Chris Martinez. I built this site to help people learn skills
+                                that would be needed to take the OSCP. This site is completely free and is done 
+                                purely out of my passion for web building and cybersecurity. The reason I chose
+                                the OSCP is because the purpose of the exam is to train hackers to achieve 
+                                professional skills; hence, to become a great cybersecurity expert, it goes without
+                                saying that the skills listed on this site will be important to know.
                             </p>
                         <br />
-                        <h3 className="subheading"><i>The Offensive Security Student Forum</i></h3>
-                            <p>The Student Forum is only accessible to Offensive Security students. Your forum credentials come
-                                in the email as well. On the forums you can ask questions, share resources, offer tips. 
-                                <br />
-                                Once you pass the exam, you will have access to the subforum for certificate holders.
+                        <h4><b>What Is the OSCP?</b></h4><br />
+                            <p>OSCP stands for Offensive Security Certified Professional. Offensive Security 
+                                provides a course on their site that covers the necessary skills to tackle their exam, 
+                                which consists of compromising boxes and escalating privileges. The course itself is
+                                $1499, which is a huge investment for those who wish to learn hacking, but the 
+                                OSCP certificate is highly valued in the CybSec world.
                             </p>
                         <br />
-                        <h3 className="subheading"><i>Live Support</i></h3>
-                            <p>You will have direct contact with Student Administrators. They are available to assist you 
-                                with technical issues and clarify items in the course material and exercises.
-                            </p>
-                        <br />
-                        <h3 className="subheading"><i>OSCP Exam Attempt</i></h3>
-                            <p>Included with your purchase of the PWK course is an attempt at the OSCP certification exam. 
-                                You have 120 days after the end of your lab time to take the exam. If your exam expires, you
-                                can purchase another one for 120 days. If you purchase a lab extension with an unused exam
-                                attempt, you will have 120 days after the last day of your lab extension.
-                                <br />
-                                To book the exam, use your personalized scheduling link in the email.
+                        <h4><b>How Can I Test My Skills?</b></h4><br />
+                            <p>When you feel confident after reading these pages, you can try out your hacking skills 
+                                by going to sites like HackTheBox (<i>which I recommend</i>) or TryHackMe. Try these out with 
+                                as little help as possible. Once you feel comfortable tackling boxes, try finding some
+                                Capture the Flag (CTF) games and grind those out until you feel skillful enough to try 
+                                the OSCP exam itself.
                             </p>
                     </div>
                 )
-            case '1.2':
+            case 'kali_linux':
                 return (
-                    <div className="chapter-01-content helvetica-text">
-                        <h2>1.2: Overall Strategies for Approaching Course</h2><br />
-                            <p>The following is recommended as a general approach to the course materials:</p>
-                            <ul>
-                                <li>Review all information in welcome and course information emails</li>
-                                <li>Review the course materials</li>
-                                <li>Complete all the course exercises</li>
-                                <li>Attack the lab machines <i>(*Note: I will cover this when we get there)</i></li>
-                            </ul>
-                            <br />
-                        <h3 className="subheading"><i>Welcome and Course Information Emails</i></h3>
-                            <p>Be sure to read these emails because as you saw in 1.1, they contain a lot 
-                                of necessary links and credentials for your course.
+                    <div className="kali_linux-page helvetica-text page">
+                        <h1>Kali Linux</h1><br />
+                        <p>Kali Linux is a Debian-based operating system developed and maintained by Offensive Security.
+                            It comes loaded with tools used for the average penetration test. These tools include: 
+                            <i> Metasploit, Nmap, Wireshark, and Aircrack-ng</i> to name a few. 
+                        </p>
+                        <br />
+                        <h4><b>Booting Up Kali Linux</b></h4><br />
+                            <p>Begin by downloading the official Kali Linux image and virtual machine software of your choosing.
+                                As a security professional, you should get in the habit of verifying every file you download to 
+                                prohibit unnecessary risk.
+                            </p>
+                            <p>The default credentials for Kali is the word 'kali' for both username and password.
+                                Once you are logged in, type <code>passwd</code> to change the password. Be careful
+                                not to log in as 'root', as root user is unrestricted and a stray command could damage
+                                the machine. If you need elevated privileges for a command, prepend it with the word 
+                                <code>sudo</code>. For example: <code>sudo apt-get install [package]</code>
                             </p>
                         <br />
-                        <h3 className="subheading"><i>Course Materials</i></h3>
-                            <p>The course comes with a lab guide and course videos. It's advised to use both resources, 
-                                opting first with whichever matches your learning style, but nonetheless using both to 
-                                synthesize your learning. Treat the course as a marathon and not a sprint; don't be 
-                                afraid to spend more time on difficult parts before advancing in the course.
+                        <h4><b>The Kali Menu</b></h4><br />
+                            <p>Navigate the menu to view your applications, which are sorted out by their primary
+                                roles. Take some time to familiarize yourself with the menu.
                             </p>
-                        <br />
-                        <h3 className="subheading"><i>Course Exercises</i></h3>
-                            <p>It is recommended that you fully complete the exercises provided at the end of each module 
-                                prior to moving on to the next module. They will test your understanding and build confidence
-                                moving forward. <u>Note:</u> some of these exercises are going to be difficult and will require
-                                a significant amount of time. It is encouraged to be persistent, especially on tougher exercises.
-                                OffSec encourages a "Try Harder" mindset.
-                            </p>
-                        <br />
-                        <h3 className="subheading"><i>PWK Labs</i></h3>
-                            <p>Once you complete the course material, you should be confident in tackling the labs with 
-                                the goal of compromising the machines and escalating privileges. These are just my notes, so I 
-                                cannot give you access to these labs; however, there are <b>FREE</b> resources that I recommend
-                                to test your skills in compromising machines. These include &nbsp;
-                                <a _target="blank" href="https://www.hackthebox.com/" className="subsection">HackTheBox</a> and
-                                <a _target="blank" href="https://tryhackme.com/" className="subsection">TryHackMe</a>.
-                            </p>
-                    </div>
-                )
-            case '1.3':
-                return (
-                    <div className="chapter-01-content helvetica-text">
-                        <h2>1.3: Obtaining Support</h2><br />
-                            <p>The PWK is not a fixed-pace course. You can go at your own pace, spending additional
-                                time on topics that are difficult for you. Don't be afraid of wrestling harder topics.
-                                <br /><br />
-                                There are times when it is appropriate, however, to contact support. Please understand
-                                that support will expect that you have gone over all course materials before trying to 
-                                tackle the labs and will not hesitate to refer you back to the course materials if 
-                                needed. Check Offensive Security's FAQ, and if no solution is found try the Student Forums.
-                                <br /><br />
-                                If you still need help, you can contact Student Administrators by visiting Live Support
-                                or by sending an email.
-                            </p>
-                    </div>
-                )
-            case '1.4':
-                return (
-                    <div className="chapter-01-content helvetica-text">
-                        <h2>1.4: About Penetration Testing</h2><br />
-                            <p>A <u>pentration test</u> is an ongoing cycle of research and attack against
-                            a target or boundary. The attack should be <b>structured, calculated, </b>and, when 
-                            possible, verified in a lab before being implemented on a live target. The
-                            cycle is pictured as such:</p>
                             <div className="text-center">
-                                <img src='/oscp/figure_1_4_1.png' className="figure-1-4-1" alt='pentest diagram' />
-                                <p><i>Figure 1: Diagram of Pentest Methodology</i></p>
+                                <img src='/oscp/kali-menu.jpg' className="IMG_kali-menu" alt='kali menu' />
+                                <p><i>The Kali Menu</i></p>
                             </div>
-                            <p>As the model suggests, the more information you gather, the higher the probability
-                                of a successful penetration test. Once initial target is penetrated, you would
-                                typically start the cycle again. For example, you might gather information 
-                                about the internal network in order to penetrate deeper.
+                        <br />
+                        <h4><b>Kali Documentation</b></h4><br />
+                            <p>Below are some resources to learn more about Kali Linux:</p>
+                            <ul>
+                                <li><a href="http://docs.kali.org" target="_blank">The Kali Linux Official Documentation</a></li>
+                                <li><a href="https://forums.kali.org" target="_blank">The Kali Linux Support Forums</a></li>
+                                <li><a href="https://tools.kali.org" target="_blank">The Kali Linux Tools Site</a></li>
+                                <li><a href="https://bugs.kali.org" target="_blank">The Kali Linux Bug Tracker</a></li>
+                                <li><a href="https://kali.training" target="_blank">The Kali Linux Training</a></li>
+                            </ul>
+                        <br />
+                        <h4><b>Finding Your Way Around Kali</b></h4><br />
+                            <p><b><u>Important Directories</u></b></p>
+                            <p>Below are the important Linux directories every user should know:</p>
+                            <ul>
+                                <li><b>/bin</b> - basic programs like <code>ls cd cat</code></li>
+                                <li><b>/sbin</b> - system programs like <code>fdisk mkfs sysctl</code></li>
+                                <li><b>/etc</b> - configuration files</li>
+                                <li><b>/tmp</b> - temporary files (typically deleted on boot)</li>
+                                <li><b>/usr/bin</b> - applications like nmap, apt, ncat</li>
+                                <li><b>/usr/share</b> - application support and data files</li>
+                            </ul>
+                            <p><u><b>Man</b></u></p>
+                            <p>Most commands in Linux distros come with bits of formal documentation. If you're ever curious 
+                                about a command, type <code>man [command]</code> to open up the documentation page for the 
+                                command.
                             </p>
+                            <p><b><u>Commands You Should Know</u></b></p>
+                            <p>If you are new to Linux, you should take some time to familiarize yourself with the 
+                                following commands:<br /> <code>ls, cd, mkdir, man, touch, rm</code>
+                            </p>
+                            <p><b><u>Finding Files in Kali Linux</u></b></p>
+                            <p>The three most common commands for finding files are <i>find, locate, and which</i></p>
+                            <ul>
+                                <li><code>which</code>: searches through directories defined in $PATH for a given file name.
+                                If a match is found, <b>which</b> will return the full path.</li>
+                                <li><code>locate</code>: the quickest of the three, locate uses a local database to find file
+                                location</li>
+                                <li><code>find</code>: most flexible and complex of the three. The code below performs 
+                                a recursive search in the root directory for any file that starts with 'sbd'</li>
+                                <p><code>find / -name sbd*</code></p>
+                            </ul>
+                        <br />
+                        <h4><b>Managing Kali Linux Services</b></h4><br />
+                    </div>
+                )
+            case 'the_command_line':
+                return (
+                    <div className="introductions-page helvetica-text">
+                        <h2>The Command Line</h2>
+                    </div>
+                )
+            case 'common_tools':
+                return (
+                    <div className="introductions-page helvetica-text">
+                        <h2>Common Tools</h2>
+                    </div>
+                )
+            case 'bash_scripting':
+                return (
+                    <div className="introductions-page helvetica-text">
+                        <h2>Bash Scripting</h2>
                     </div>
                 )
         }
